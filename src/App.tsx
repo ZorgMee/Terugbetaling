9a48d2dc2ea670aaffb68654cb77839076530f23
Friedl Rubrecht <friedl_rubrecht@hotmail.com>
2023-01-05 23:01:27 +0100

    add footer and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import './App.scss';
 import MainQuiz from "./MainQuiz";
-import {Col, Container, Row} from "react-bootstrap";
+import logo from './zm-logo.svg'
+import {Card, Col, Container, Row} from "react-bootstrap";
 
 function App() {
     return (
         <div className="App">
-            <Container>
-                <Row>
-                    <Col>
-                        <MainQuiz/>
-                    </Col>
-                </Row>
-            </Container>
+            <Card>
+                <Card.Header>Bereken jouw terugbetaling</Card.Header>
+                <Card.Body>
+                    <MainQuiz/>
+                </Card.Body>
+                <Card.Footer className="Footer">
+                    <Container>
+                        <Row>
+                            <Col xs={2}>
+                                <img className="App-logo" src={logo} alt="ZorgMee"/>
+                            </Col>
+                            <Col style={{margin: "auto"}}>
+                                {"Deze tool gratis op jouw website? Klik "}
+                                <a className="link-primary" href="https://www.zorgmee.be">hier</a>
+                                {" voor meer info."}
+                            </Col>
+                        </Row>
+                    </Container>
+                </Card.Footer>
+            </Card>
+
         </div>
     );
 }
